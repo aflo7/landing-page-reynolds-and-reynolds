a04a32b7f20095a1a0cf1525f1096dee491be9c0
Andres Flores <andresflo883@gmail.com>
2023-12-11 09:12:03 -0500

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
 import './App.css';
 import reynoldsAndReynolds from './assets/reynolds-and-reynolds.jpeg';
 import Divider from '@mui/material/Divider';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
